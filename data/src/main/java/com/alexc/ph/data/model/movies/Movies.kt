c8164d8ddf83026f8b41b9c00e2c76b942e62747
alexanderroca <alexph.dev@gmail.com>
2024-10-05 00:08:30 +0800

    add default value to ignore serialization

--- data/src/main/java/com/alexc/ph/data/model/movies/Movies.kt
+++ data/src/main/java/com/alexc/ph/data/model/movies/Movies.kt
@@ -11,25 +11,25 @@ data class Dates(
 
 @Serializable
 data class Movie(
-    val adult: Boolean,
-    @SerialName("backdrop_path")  val backDropPath: String,
-    @SerialName("genre_ids") val genreIds: List<Int>,
     val id: Int,
-    @SerialName("original_language") val originalLanguage: String,
-    @SerialName("original_title") val originalTitle: String,
-    val overview: String,
-    val popularity: Double,
-    @SerialName("poster_path") val posterPath: String,
-    @SerialName("release_date") val releaseDate: String,
     val title: String,
-    val video: Boolean,
-    @SerialName("vote_average") val voteAverage: Double,
-    @SerialName("vote_count") val voteCount: Int
+    val video: Boolean = false,
+    val adult: Boolean = false,
+    val overview: String = "",
+    val popularity: Double = .0,
+    @SerialName("genre_ids") val genreIds: List<Int> = emptyList(),
+    @SerialName("original_language") val originalLanguage: String = "",
+    @SerialName("original_title") val originalTitle: String = "",
+    @SerialName("backdrop_path")  val backDropPath: String = "",
+    @SerialName("poster_path") val posterPath: String = "",
+    @SerialName("release_date") val releaseDate: String = "",
+    @SerialName("vote_average") val voteAverage: Double = .0,
+    @SerialName("vote_count") val voteCount: Int = 0
 )
 
 @Serializable
 data class Movies(
-    val dates: Dates,
+    val dates: Dates? = null,
     val page: Int,
     @SerialName("results") val movies: List<Movie>,
     @SerialName("total_pages") val totalPages: Int,
