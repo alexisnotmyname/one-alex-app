5a4e7c3a60203c8e4f8c792344985a5d8b46a218
alexanderroca <alexph.dev@gmail.com>
2025-02-16 10:53:58 +0800

    update todo schema

--- domain/src/main/java/com/alexc/ph/domain/model/TodoItem.kt
+++ domain/src/main/java/com/alexc/ph/domain/model/TodoItem.kt
@@ -4,6 +4,9 @@ data class TodoItem(
     val id: Int = 0,
     val title: String,
     val isDone: Boolean = false,
-    val order: Int
+    val order: Int,
+    val dateTimeCreated: Long = System.currentTimeMillis(),
+    val dateTimeDue: Long? = null,
+    val dateTimeCompleted: Long? = null
 )
 
